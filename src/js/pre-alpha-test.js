6dde2e38b04e9ef3d29796806d4502fe23297a04
ReCen20 <alpha55757@gmail.com>
2020-01-04 16:34:46 +0400

    Alpha 1

--- src/js/pre-alpha-test.js
+++ src/js/pre-alpha-test.js
@@ -1,7 +1,20 @@
 "use strict"
 
-let admin;
-let name;
-name = "Джон";
-admin = name;
-alert(admin);
+let userName;
+let password;
+userName = prompt("Who are your?");
+if (userName == "Админ") {
+
+    password = prompt("Enter the password")
+    if( password == "Я главный") {
+        alert("Дарова");
+    } else if (!password || password == null) {
+        alert("Беда. Говорить наверное не умеем");
+    } else {
+        alert("Фраер!")
+    }
+} else if (!userName || userName == null) {
+    alert("Беда. Говорить наверное не умеем");
+} else {
+    alert("Фраер!")
+}
